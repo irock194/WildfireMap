c347fda08f7982e87bed97b3efe81eef20f2ba95
Jeffrey Ong <kwanqing.ong@sjsu.edu>
2021-11-03 19:28:50 -0700

    Update

--- wildfiremap/src/components/Header.js
+++ wildfiremap/src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Icon } from '@iconify/react'
 import alertIcon from '@iconify/icons-clarity/alert-solid'
+import SearchBar from './SearchBar';
 
 const Header = () => {
     return (
         <header className="header">
             <h1><Icon icon={alertIcon} /> Disaster Tracker </h1>
+            <h2><SearchBar /> </h2>
         </header>
     )
 }
--- wildfiremap/src/components/SearchBar.js
+++ wildfiremap/src/components/SearchBar.js
@@ -1,12 +1,13 @@
 const SearchBar = () => (
     <form action="/" method="get">
         <label htmlFor="header-search">
-            <span className="visually-hidden">Search blog posts</span>
+            <span className="visually-hidden">Search </span>
         </label>
         <input
+            className="box"
             type="text"
             id="header-search"
-            placeholder="Search blog posts"
+            placeholder="Type to Search..."
             name="s" 
         />
         <button type="submit">Search</button>
