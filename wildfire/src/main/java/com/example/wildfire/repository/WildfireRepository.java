d652e5139042d554cf7c10760f16e78fd888ed25
irock194 <gabrieltenocelotl@gmail.com>
2021-11-16 18:46:04 -0800

    minor changes

--- wildfire/src/main/java/com/example/wildfire/repository/WildfireRepository.java
+++ wildfire/src/main/java/com/example/wildfire/repository/WildfireRepository.java
@@ -2,9 +2,11 @@ package com.example.wildfire.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.wildfire.entity.*;
 
+@CrossOrigin(origins="http://localhost:3000")
 @Repository
 public interface WildfireRepository extends JpaRepository<Wildfire, Long>{
 
--- wildfiremap/src/App.js
+++ wildfiremap/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import Map from './components/Map'
 import Loader from './components/Loader'
 import Header from './components/Header'
+import axios from "axios";
 
 function App() {
   const [eventData, setEventData] = useState([])
@@ -10,9 +11,13 @@ function App() {
   useEffect(() => {
     const fetchEvents = async () => {
       setLoading(true)
-      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events') //process.env.WILDFIRE_ENV
-      // console.log(process.env.WILDFIRE_ENV)
-      // const res = await fetch(process.env.WILDFIRE_ENV)
+      const res = await fetch('http://10.0.0.135:5000/getNASA',{
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }}) //process.env.WILDFIRE_ENV
+
+      // const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
       const { events } = await res.json()
 
       setEventData(events)
